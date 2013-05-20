e2094e32b4e267694410edf45ef75d0b9f536ee6
Nick Doiron <ndoiron@mapmeld.com>
2013-05-20 07:45:00 -0700

    add new button keyword to help terms

--- public/scripts/helpterms.js
+++ public/scripts/helpterms.js
@@ -3,5 +3,6 @@ var helpTerms = [
   { name: "marker", about: 'Put a red pin on the map' },
   { name: "line", about: 'Two or more points make a line.<br/>You can write a coordinate such as [ 0, 0 ] or a location such as @ "Buenos Aires"' },
   { name: "shape", about: 'Three or more points make a shape.<br/>You can write a coordinate such as [ 0, 0 ] or a location such as @ "Buenos Aires"' },
-  { name: "@", about: '@ can look up a city, state, or address. For example, @ "Boston"' }
+  { name: "@", about: '@ can look up a city, state, or address. For example, @ "Boston"' },
+  { name: "button", about: 'Put markers, lines, and shapes between button and plz to control them by a button. You can add a label to a button like "Click Me".' }
 ];
